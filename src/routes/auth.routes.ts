11c092f437b57e13372a3f9fd6eeaa2a3de69dd7
hrmdbipolar <hrmandr@gmail.com>
2023-05-23 18:44:35 +0300

    route-correcting

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/profile', userController.getUserProfile);
 router.post('/authenticate', userController.authenticateUser);
 
 export default router;
